34c2da9cebe33c69defcd247491098bfebe5fa4f
Scottlr <xscott@hotmail.co.uk>
2016-05-01 13:31:13 +0100

    Inital work

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,30 @@
-#[cfg(test)]
-mod test {
-    #[test]
-    fn it_works() {
-    }
+use std::io::prelude::*;
+use std::fs::File;
+use std::path::Path;
+
+fn main()
+{
+	let file_contents = match read_file("lange.fir")
+	{
+		Ok(contents) => contents,
+		Err(err) => panic!("failed reading file: {}", err)
+	};
+
+	for c in file_contents
+	{
+		println!("{}", c);
+	}
+
+}
+
+fn read_file<P: AsRef<Path>>(path: P) -> Result<Vec<char>, String>
+{	
+	File::open(path)
+		.map_err(|err| err.to_string())
+		.and_then(|mut file| {
+			let mut contents = String::new();
+			file.read_to_string(&mut contents)
+				.map_err(|err| err.to_string())
+				.map(|_| contents.chars().collect())
+		})
 }
