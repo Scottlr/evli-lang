2e89544b093cf6ed646ef0e9fafcbc22c3d84a6b
Scottlr <xscott@hotmail.co.uk>
2016-05-05 15:42:47 +0100

    replacing default tabs with spaces for better formatting

--- src/main.rs
+++ src/main.rs
@@ -6,26 +6,22 @@ use std::path::Path;
 mod lexer;
 
 fn main() {
-	let file_contents = match read_file("lange.fir") {
-		Ok(contents) => contents,
-		Err(err) => panic!("failed reading file: {}", err)
-	};
+    let file_contents = match read_file("lange.fir") {
+        Ok(contents) => contents,
+        Err(err) => panic!("failed reading file: {}", err)
+    };
 	
-	println!("{}", file_contents);
-	lexer::tokenize_generic(&file_contents);
-
-
-	
-
+    println!("{}", file_contents);
+    lexer::tokenize_generic(&file_contents);
 }
-
+    
 fn read_file<P: AsRef<Path>>(path: P) -> Result<String, String> {	
-	File::open(path)
-		.map_err(|err| err.to_string())
-		.and_then(|mut file| {
-			let mut contents = String::new();
-			file.read_to_string(&mut contents)
-				.map_err(|err| err.to_string())
-				.map(|_| contents.to_string())
-		})
+    File::open(path)
+        .map_err(|err| err.to_string())
+        .and_then(|mut file| {
+            let mut contents = String::new();
+            file.read_to_string(&mut contents)
+                .map_err(|err| err.to_string())
+                .map(|_| contents.to_string())
+        })
 }
