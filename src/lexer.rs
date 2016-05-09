a14a9568db72aff9cb133b5249723377b3b00d95
Scottlr <xscott@hotmail.co.uk>
2016-05-09 20:49:51 +0100

    Finally have regex working and capturing string tokens!

--- src/lexer.rs
+++ src/lexer.rs
@@ -12,9 +12,18 @@ fn phrase_to_token(captured_phrase: &str) -> Token {
 pub fn tokenize<'a>(source_code: &'a str) -> Vec<Token>
 {
     let mut captured_tokens = vec![];
-    let regex_exp = Regex::new("({|}|(|)|;)|\".+?\"|.+?(;| |(|)\\n)").unwrap();
+    println!("In tokenize method... attempting to create regex object");
+
+    let regex_exp = match Regex::new("(\\*)|\".+?\"|.+?\\*") {
+        Ok(res) => res,
+        Err(err) => panic!("{}", err)
+    };
+
+ 
+    println!("Created object...");
+    
     for captured_token in regex_exp.captures_iter(source_code) {
-        println!("Token captured: {:?}", captured_token);
+        println!("Token captured: {:?}", captured_token.at(0));
     }
 
     captured_tokens
