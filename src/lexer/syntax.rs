be0309bf3fde3d19942a335be0e50ecf8f59682e
Scottlr <xscott@hotmail.co.uk>
2016-05-17 21:31:58 +0100

    started moving syntax functionality into a struct

--- src/lexer/syntax.rs
+++ src/lexer/syntax.rs
@@ -1,39 +1,48 @@
 pub static OPERATORS: &'static [char] = 
     &['=', '(', ')', '+', '-', '{', '}', ';', '>', '<', ',', ':', '*'];
 
-pub fn map_operator_token(phrase: &str) -> Token {
-    match phrase {
-        ";" => Token::SemiColon,
-        "(" => Token::OpenParen,
-        ")" => Token::CloseParen,
-        "=" => Token::Equals,
-        "{" => Token::OpenBrace,
-        "}" => Token::CloseBrace,
-        ">" => Token::RightAngle,
-        "<" => Token::LeftAngle,
-        "-" => Token::Hyphen,
-        "," => Token::Comma,
-        ":" => Token::Colon,
-        "*" => Token::Asterix,
-        "+" => Token::Plus,
-        "/" => Token::ForwardSlash,
-        "\\" => Token::BackSlash,
-        _ => panic!("Didn't recognise phrase to parse to token...")
-    }
-} 
+struct Syntax;
 
-pub fn map_compound_operator_token(current_token: Token, next_token: Token) -> Option<Token> {
-    match (current_token, next_token) {
-        (Token::Plus, Token::Equals) => Some(Token::PlusEquals),
-        (Token::Hyphen, Token::Equals) => Some(Token::MinusEquals),
-        (Token::Asterix, Token::Equals) => Some(Token::MultiplicationEquals),
-        (Token::BackSlash, Token::Equals) => Some(Token::DivideEquals),
-        _ => None
+impl Syntax {
+    pub fn map_operator_token(phrase: &str) -> Token {
+        match phrase {
+            ";" => Token::SemiColon,
+            "(" => Token::OpenParen,
+            ")" => Token::CloseParen,
+            "=" => Token::Equals,
+            "{" => Token::OpenBrace,
+            "}" => Token::CloseBrace,
+            ">" => Token::RightAngle,
+            "<" => Token::LeftAngle,
+            "-" => Token::Hyphen,
+            "," => Token::Comma,
+            ":" => Token::Colon,
+            "*" => Token::Asterix,
+            "+" => Token::Plus,
+            "/" => Token::ForwardSlash,
+            "\\" => Token::BackSlash,
+            _ => panic!("Didn't recognise phrase to parse to token...")
+        }
+    } 
+
+    pub fn map_compound_operator_token(current_token: Token, next_token: Token) -> Option<Token> {
+        match (current_token, next_token) {
+            (Token::Plus, Token::Equals) => Some(Token::PlusEquals),
+            (Token::Hyphen, Token::Equals) => Some(Token::MinusEquals),
+            (Token::Asterix, Token::Equals) => Some(Token::MultiplicationEquals),
+            (Token::BackSlash, Token::Equals) => Some(Token::DivideEquals),
+            _ => None
+        }
     }
 }
 
-pub enum Token
-{
+
+pub enum TokenType {
+    Operator,
+    CompoundOpeartor
+}
+
+pub enum Token {
     SemiColon,
     OpenParen,
     CloseParen,
