6ceeca2be053898dd6b415c1b476da8c9df0ab80
Scottlr <xscott@hotmail.co.uk>
2016-05-29 14:03:20 +0100

    Further refactoring with traits and another level of abstraction on token parsers

--- src/lexer/syntax/tokenparser.rs
+++ src/lexer/syntax/tokenparser.rs
@@ -4,19 +4,20 @@ use super::syntaxparser::Parser;
 pub struct TokenParser;
 
 impl Parser for TokenParser {
-    fn parse(source_code: &mut SlidingWindow) -> Token {
+    fn parse(self, source_code: &mut SlidingWindow) -> Token {
         let phrase = source_code.current_character();
-        match phrase {
-            ';' => Token::SemiColon,    '(' => Token::OpenParen,
-            ')' => Token::CloseParen,   '=' => Token::Equals,
-            '{' => Token::OpenBrace,    '}' => Token::CloseBrace,
-            '>' => Token::RightAngle,   '<' => Token::LeftAngle,
-            '-' => Token::Hyphen,       ',' => Token::Comma,
-            ':' => Token::Colon,        '*' => Token::Asterix,
-            '+' => Token::Plus,         '/' => Token::ForwardSlash,
-            '\\' => Token::BackSlash,   '\"' => Token::QoutationMark,
-            _ => panic!("Didn't recognise phrase to parse to token... {}", phrase)
+        let token = map_token(&phrase);
+
+        match token {
+            Token::Plus | Token::Hyphen |
+            Token::BackSlash | Token::Asterix => {
+                let next_character = source_code.peek().to_string();
+                let next_token = self.map_token(&next_character);
+            }
+
         }
+
+
     } 
  
 }
@@ -31,6 +32,20 @@ impl TokenParser {
             (Token::BackSlash, Token::Equals) => Token::DivideEquals,
             _ => panic!("Invalid combinations for compound token...")
         }
-        
     }
+
+    pub fn map_token(&self, character: &char) -> Token {
+         match *character {
+            ';' => Token::SemiColon,    '(' => Token::OpenParen,
+            ')' => Token::CloseParen,   '=' => Token::Equals,
+            '{' => Token::OpenBrace,    '}' => Token::CloseBrace,
+            '>' => Token::RightAngle,   '<' => Token::LeftAngle,
+            '-' => Token::Hyphen,       ',' => Token::Comma,
+            ':' => Token::Colon,        '*' => Token::Asterix,
+            '+' => Token::Plus,         '/' => Token::ForwardSlash,
+            '\\' => Token::BackSlash,   '\"' => Token::QoutationMark,
+            _ => { panic!("ERROR")}
+        }
+    }
+
 }
