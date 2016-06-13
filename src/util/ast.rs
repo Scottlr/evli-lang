c24de38879690e3c9cca274e922f1ef034a39e98
Scottlr <xscott@hotmail.co.uk>
2016-06-13 19:25:41 +0100

    further work on ast

--- src/util/ast.rs
+++ src/util/ast.rs
@@ -10,3 +10,17 @@ struct AbstractSyntaxTree {
     rootNode: SyntaxNode;
 }
 
+pub trait SyntaxVisitor {
+    fn evaluate_let(&mut self, node: SyntaxNode);
+    fn evaluate_openbrace(&mut self, node: SyntaxNode);
+} 
+
+impl AbstractSyntaxTree {
+    pub fn parse(self, tokens: Vec<Token>) -> AbstractSyntaxTree {
+        AbstractSyntaxTree
+    }
+}
+
+impl SyntaxVisitor for AbstractSyntaxTree {
+    
+}
