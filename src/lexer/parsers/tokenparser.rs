4980e698addb48a83e846e097ad3aad496c66f97
Scottlr <xscott@hotmail.co.uk>
2016-06-14 22:06:40 +0100

    formatting + unit tests for new compound tokens

--- src/lexer/parsers/tokenparser.rs
+++ src/lexer/parsers/tokenparser.rs
@@ -13,6 +13,7 @@ impl Parser for TokenParser {
             source_code.increase_offset();
             let next_character = source_code.offset_peek();
            
+            //TODO: Really shouldn't clone the token, revise
             if let Some(parsed_next_token) = self.map_token(next_character) {
                 if let Some(parsed_compound_token) = self.map_compound_token(token.clone(), parsed_next_token) {
                     token = parsed_compound_token;
@@ -41,21 +42,23 @@ impl TokenParser {
 
     pub fn map_token(&self, character: char) -> Option<Token> {
          match character {
-            ';' =>  Some(Token::SemiColon),    '(' => Some(Token::OpenParen),
-            ')' =>  Some(Token::CloseParen),   '=' => Some(Token::Equals),
-            '{' =>  Some(Token::OpenBrace),    '}' => Some(Token::CloseBrace),
-            '>' =>  Some(Token::RightAngle),   '<' => Some(Token::LeftAngle),
-            '-' =>  Some(Token::Hyphen),       ',' => Some(Token::Comma),
-            ':' =>  Some(Token::Colon),        '*' => Some(Token::Asterix),
-            '+' =>  Some(Token::Plus),         '/' => Some(Token::ForwardSlash),
-            '\\' => Some(Token::BackSlash),   '\"' => Some(Token::QoutationMark),
-            ' ' =>  Some(Token::Whitespace),   '\n' => Some(Token::NewLine),
+            ';' =>  Some(Token::SemiColon),     '(' =>  Some(Token::OpenParen),
+            ')' =>  Some(Token::CloseParen),    '=' =>  Some(Token::Equals),
+            '{' =>  Some(Token::OpenBrace),     '}' =>  Some(Token::CloseBrace),
+            '>' =>  Some(Token::RightAngle),    '<' =>  Some(Token::LeftAngle),
+            '-' =>  Some(Token::Hyphen),        ',' =>  Some(Token::Comma),
+            ':' =>  Some(Token::Colon),         '*' =>  Some(Token::Asterix),
+            '+' =>  Some(Token::Plus),          '/' =>  Some(Token::ForwardSlash),
+            '\\' => Some(Token::BackSlash),     '\"' => Some(Token::QoutationMark),
+            ' ' =>  Some(Token::Whitespace),    '\n' => Some(Token::NewLine),
             '\r' => Some(Token::CarraigeReturn),
             _ => None
         }
     }
 
 }
+
+
 
 #[cfg(test)]
 mod tests {
@@ -93,6 +96,9 @@ mod tests {
         assert_eq!(parser_helper("*="), Token::MultiplicationEquals);
         assert_eq!(parser_helper("/="), Token::DivideEquals);
         assert_eq!(parser_helper("=="), Token::ConditionalEquals);
+        assert_eq!(parser_helper("++"), Token::IncrementOperator);
+        assert_eq!(parser_helper("--"), Token::DecrementOperator);
+        assert_eq!(parser_helper("->"), Token::PointerArrow);
     }
 
     #[test]
